b50f090d768b148faa7cd7ef7481f75864f223ad
maria lopez <ferchislopez0910@gmail.com>
2021-02-09 07:48:41 +0000

    correc 4 and 6 pint

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
-*_isalpha - checks for alphabetic character.
-*@c: character one lettre
-*Return: 1 if int c is lowercase, 0 if otherwise
+*_isalpha - checks for alphabetic .
+*@c: character
+*Return: 1 if c is a letter, lowercase or upper, 0 if otherwise
 */
 int _isalpha(int c)
 {
-if ((c >= 'a' && c <= 'z') || (c >= 'Z' && c <= 'Z'))
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 return (1);
 else
 return (0);
