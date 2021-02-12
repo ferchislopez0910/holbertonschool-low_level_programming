7d66c094762721e0fe3f5518ff817a9ac7d86f39
maria lopez <ferchislopez0910@gmail.com>
2021-02-12 04:01:37 +0000

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,17 +2,28 @@
 
 /**
 *more_numbers - print to number of 1 -14
-* 
+*
 * return 0
 */
 
 void more_numbers(void)
 {
-	int a;
+	int a, b, c, i;
 
-	for (a = 0; a <= 14 ; a++)
-	_putchar(a);
-{
-	_putchar('\n');
-}
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c < 10)
+				b = c;
+			else
+			{
+				a = c / 10;
+				b = c % 10;
+				_putchar('0' + a);
+			}
+			_putchar('0' + b);
+		}
+		_putchar('\n');
+	}
 }
