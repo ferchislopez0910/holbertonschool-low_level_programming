62b6257f2a1899cdf07deb27ed04106e8c43e2b5
maria lopez <ferchislopez0910@gmail.com>
2021-02-12 03:07:20 +0000

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+*more_numbers - print to number of 1 -14
+*
+* return 0
+*/
+
+void more_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 14 ; a++)
+	_putchar(a);
+{
+	_putchar('\n');
+}
+}
