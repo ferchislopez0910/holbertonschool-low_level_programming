6d48f0af037e1589774ac6c84250e5485e72881c
maria lopez <ferchislopez0910@gmail.com>
2021-02-12 01:54:47 +0000

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <unistd.h>
+
+/**
+* print_numbers - product of 2 numbers.
+*
+* return 0
+*/
+
+void print_numbers(void)
+{
+	int a;
+for (a = '0'; a <= '9' ; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
