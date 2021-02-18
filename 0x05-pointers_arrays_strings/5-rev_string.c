b0fdf618dfb2c8bcb0f51efc630725d49568af29
maria lopez <ferchislopez0910@gmail.com>
2021-02-18 01:09:51 +0000

    5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,24 +1,27 @@
 #include "holberton.h"
 
 /**
-* rev_string - function that prints a string.
+* rev_string - actualizar el arreglo de forma inversa.
 *
 * @s: its a ptr
 *
+* Return: 0.
 */
 
 void rev_string(char *s)
 {
 	int a = 0;
 	int b;
+	char c;
 
 	while (s[a] != '\0')
 	{
 		a++;
 	}
-	for (b = a - 1; b >= 0; b--)
-		{
-		_putchar(s[b]);
+	for (b = 0,  a = a - 1; b < a; b++, a--)
+	{
+		c = s[b];
+		s[b] = s[a];
+		s[a] = c;
 	}
-		_putchar('\n');
 }
