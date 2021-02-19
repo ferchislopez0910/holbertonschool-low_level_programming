df1959a5a7265693498ed7e7d8b95004798553ba
maria lopez <ferchislopez0910@gmail.com>
2021-02-19 01:01:37 +0000

    1-strcat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+* _strncat - function that concatenates two strings.
+*
+* @dest: destino
+* @src: source
+* @n: espacio requerido
+* Return: 0.
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	for (a = 0; dest[a] != '\0'; a++)
+	{}
+
+	for (b = 0; b < n && src[b] != '\0'; b++)
+	{
+		*(dest + a) = *(src + b);
+	}
+	return (dest);
+}
