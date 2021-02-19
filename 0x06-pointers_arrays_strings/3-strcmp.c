0fec8dec59d3e0d1ade65825eceb0f907e3aabf1
maria lopez <ferchislopez0910@gmail.com>
2021-02-19 03:24:01 +0000

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,13 +10,7 @@
 int _strcmp(char *s1, char *s2)
 {
 	int a;
-	int b;
-	int c;
 
-	for (b = 0; s1[b] != '\0'; b++)
-	{}
-	for (c = 0; s2[c] != '\0'; c++)
-	{}
 	for (a = 1; s1[a] != '\0' && s2[a] != '\0'; a++)
 	{
 		if (s1[a] < s2[a])
