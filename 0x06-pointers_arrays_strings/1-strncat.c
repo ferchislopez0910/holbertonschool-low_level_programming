47e17b4c83f875a051925d70e48a1f1e4734ae3a
maria lopez <ferchislopez0910@gmail.com>
2021-02-19 02:01:33 +0000

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,7 +17,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (a = 0; dest[a] != '\0'; a++)
 	{}
 
-	for (b = 0; b < n && src[b] != '\0'; b++)
+	for (b = 0; b < n && src[b] != '\0';)
 	{
 		*(dest + a) = *(src + b);
 		a++;
