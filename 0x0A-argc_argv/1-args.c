57c46992c8471e3012b9f5774f3006df931cbee6
ferchislopez <ferchislopez0910@gmail.com>
2021-02-27 23:57:19 -0500

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: contador
+ * @argv: matriz
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	if (argv[argc] != '\0')
+	{}
+	return (0);
+}
