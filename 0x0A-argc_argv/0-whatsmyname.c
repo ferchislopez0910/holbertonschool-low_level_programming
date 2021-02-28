3163c33f47792ee79d177b009b05a4d4dd13d81e
ferchislopez <ferchislopez0910@gmail.com>
2021-02-27 23:44:22 -0500

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: contador
+ * @argv: matriz
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	if (argc > 1)
+	{}
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,8 @@
+**0x0A-argc_argv/**
+
+*In this project we will look at topics such as:*
+
+*argc 
+*argv
+
+
