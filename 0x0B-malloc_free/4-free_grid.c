90821fdb336c88ee07709c16bab3c9f0c8222ab3
ferchislopez <ferchislopez0910@gmail.com>
2021-03-03 22:41:57 -0500

    4-free_grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - prints a grid of integers
+ * @grid: ptr
+ * @height: height of the grid
+ * Return: Nothing.
+ */
+void free_grid(int **grid, int height)
+{
+
+	int i;
+
+	for (i = 0; i < height ; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
