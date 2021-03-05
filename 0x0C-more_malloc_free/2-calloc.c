dac4b3b1c57888ca009c867bffea97e71ae6e71d
ferchislopez <ferchislopez0910@gmail.com>
2021-03-05 18:24:55 -0500

    2-calloc -  function that allocates memory for an array, using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr = malloc(size*nmemb);
 	if(ptr == 0)
 		return(NULL);
-	for(i = 0; i < nmemb; i++)
+	for(i = 0; i < (nmemb * size); i++)
 		{
 			ptr[i]= '\0';
 		}
