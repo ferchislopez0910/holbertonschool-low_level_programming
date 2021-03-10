975ebf4636694d3be2dc856771cc834af2513a33
ferchislopez <ferchislopez0910@gmail.com>
2021-03-09 19:39:41 -0500

    5-free_dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * free_dog - function that frees dogs.
+ * @d: ptr
+ * Return: void.
  */
 
 void free_dog(dog_t *d)
-{}
+{
+	if (d != '\0')
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
