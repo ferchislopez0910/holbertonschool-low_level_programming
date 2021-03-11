b42692928c74755de0cf8fd650386c9c078fa4c4
ferchislopez <ferchislopez0910@gmail.com>
2021-03-10 21:20:14 -0500

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,12 +14,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if ((array != NULL) && (size != '\0'))
+	if ((array != NULL) && (cmp != NULL))
 	{
 		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]) != 0)
-			return (i);
+			{
+				return (i);
+			}
 		}
 	}
 	else if (size <= 0)
