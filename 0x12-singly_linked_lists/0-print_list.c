e6307167b4b8c16923be0d0a7a67859788fd84b1
ferchislopez <ferchislopez0910@gmail.com>
2021-03-19 19:17:08 -0500

    correction code

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,17 +10,16 @@ size_t print_list(const list_t *h)
 {
 	int contador = 0;
 
-
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0](nil)\n");
+			printf("[0] (nil)\n");
 			h = h->next;
 		}
 		else
 		{
-			printf("[]%u %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 			h = h->next;
 		}
 	contador++;
