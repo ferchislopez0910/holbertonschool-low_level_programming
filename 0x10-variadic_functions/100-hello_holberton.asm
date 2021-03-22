e92b2d76fb745ef92c442e0e6b438d2e05ad3d67
ferchislopez <ferchislopez0910@gmail.com>
2021-03-21 22:36:20 -0500

    advance

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,15 +1,17 @@
 global main
-extern write
 
-	section .text
+section .text
 main:
-	push rbp
-	mov rdi, format
-	mov rsi, message
-	mov rax, 0
-	call write
-	pop rbp
-	mov rax, 0
-	ret
-message: db "Hello, Holberton", 0
-format:	db "%s", 10, 0
+	mov rax, 1; write(
+	mov rdi, 1; STDOUT_FILENO,
+	mov rsi, msg; "Hello, Holberton\n",
+	mov rdx, msglen ; sizeof("Hello, Holberton\n")
+	syscall ; );
+
+	mov rax, 60 ; exit(
+	mov rdi, 0; EXIT_SUCCESS
+	syscall ; );
+
+section .data
+	msg: db "Hello, Holberton", 10
+	msglen: equ $ - msg
