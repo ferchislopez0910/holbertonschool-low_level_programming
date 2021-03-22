577f9b47ca8edcf44966f9540700f2d9e8bf5934
ferchislopez <ferchislopez0910@gmail.com>
2021-03-21 22:06:24 -0500

    advance singly linked list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main_100first(void)__attribute__ ((constructor));
+/**
+ * main_100first - Write a function that prints before the main.
+ *
+ * Return: 0.
+ */
+int main_100first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+	return (0);
+}
