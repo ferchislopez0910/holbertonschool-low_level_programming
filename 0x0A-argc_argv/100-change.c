863cb64110efe6e72995ec3759349383a84d9d79
ferchislopez <ferchislopez0910@gmail.com>
2021-03-21 21:34:24 -0500

    correction Betty

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,40 +13,39 @@ int main(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
-    int cont, contador_array, cent = 0;
-    int change_[5] = {25, 10, 5, 2, 1};
-    
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    cont = atoi(argv[1]);
+int cont, contador_array, cent = 0;
+int change_[5] = {25, 10, 5, 2, 1};
 
-    if (cont < 0)
-    {
-        printf("0\n");
-        return (0);
-    }
-    else
-    {
-        for (contador_array = 0; contador_array < 5; contador_array++)
-        {
-            if (cont >= change_[contador_array])
-            {
-                cont -= change_[contador_array];
-                cent += 1;
-                if (cont >= change_[contador_array])
-                {
-                    contador_array--;
-                }
-                else if (cont == 0)
-                {
-                break;
-                }
-            }
-        }
-    printf("%d\n", cent);
-    return (0);
-    }
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+cont = atoi(argv[1]);
+if (cont < 0)
+{
+printf("0\n");
+return (0);
+}
+else
+{
+for (contador_array = 0; contador_array < 5; contador_array++)
+{
+if (cont >= change_[contador_array])
+{
+cont -= change_[contador_array];
+cent += 1;
+if (cont >= change_[contador_array])
+{
+contador_array--;
+}
+else if (cont == 0)
+{
+break;
+}
+}
+}
+printf("%d\n", cent);
+return (0);
+}
 }
