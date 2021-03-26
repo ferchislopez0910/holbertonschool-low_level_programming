276784773d0d4417d0ef7b07fc6dff2f1d8af035
ferchislopez <ferchislopez0910@gmail.com>
2021-03-25 20:40:49 -0500

    Correction

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -3,8 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "holberton.h"
+#include <stdarg.h>
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
