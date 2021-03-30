f5f2ff3644f05d0877df28ad939835ff35df8a8d
ferchislopez <ferchislopez0910@gmail.com>
2021-03-29 23:20:53 -0500

    Create a function that creates a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * append_text_to_file - Create a function that creates a file
+ * @filename: ptr file name.
+ * @text_content: ptr nro.
+ * Return: 1 | -1.
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int size, file;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+		text_content = "";
+	for (size = 0; text_content != '\0'; size++)
+	{}
+	file = open(filename, O_WRONLY | O_APPEND);
+
+	if (file == -1)
+		return (-1);
+
+	write(file, text_content, size);
+
+	return (1);
+}
