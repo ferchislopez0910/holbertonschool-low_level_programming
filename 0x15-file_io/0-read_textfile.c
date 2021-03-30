c6190fc6ad37ddc12fd5437a85b25f5e54b61a2b
ferchislopez <ferchislopez0910@gmail.com>
2021-03-29 20:10:42 -0500

    correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,6 +10,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int op, rd, wt;
 	char *buff;
 
+	if (filename == NULL)
+		return (0);
+
 	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
 		return (0);
@@ -18,22 +21,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (op == -1)
 	{
 		free(buff);
-		close(op);
 		return (0);
 	}
 	rd = read(op, buff, letters);
 	if (rd == -1)
 	{
 		free(buff);
-		close(op);
 		return (0);
 	}
 	wt = write(STDOUT_FILENO, buff, rd);
 	if (wt != rd)
 	{
 		free(buff);
-		close(op);
 		return (0);
 	}
+	free(buff);
+	close(op);
 	return (wt);
 }
