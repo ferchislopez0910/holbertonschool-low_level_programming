a5d41d99e557ee7d05f496735266728bff0dd725
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-04-30 15:31:19 -0500

    Update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,21 +1,20 @@
 #include "lists.h"
-
 /**
- * print_dlistint- singly linked list
- * @h: ptr
- * Return: number_nodos.
- */
+* print_dlistint- singly linked list
+* @h: ptr
+* Return: number_nodos.
+*/
 
 size_t print_dlistint(const dlistint_t *h)
 {
 	int number_nodos = 0;
 
 	while (h != NULL)
-   {
-      printf("%d\n", h->n);
+	{
+		printf("%d\n", h->n);
 		h = h->next;
 	}
-   number_nodos++;
+	number_nodos++;
 
 	return (number_nodos);
 }
