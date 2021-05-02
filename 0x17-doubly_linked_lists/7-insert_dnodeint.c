bf82a78baaf8699e96ae988c9e76cb650617c69f
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-05-02 17:40:31 -0500

    Insert new node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,6 +15,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *auxnew;
 
 	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 	if (*h == NULL || idx == 0)
 	{
 		new->n = n;
