73cb1fa144b8a7210d51504d487e22f1633fe9f9
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-05-02 18:41:53 -0500

    Delete node

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+* delete_dnodeint_at_index - delete the nodes
+* @headead: Ptr
+* @index: index
+* Return: Always EXIT_SUCCESS.
+*/
+int delete_dnodeint_at_index(dlistint_t **headead, unsigned int index)
+{
+	dlistint_t *tmp = *headead;
+	dlistint_t *new;
+	unsigned int i;
+
+	if (tmp == NULL)
+		return (-1);
+
+	if (index == 0)
+	{	*headead = (*headead)->next;
+		free(tmp);
+		return (1);
+	}
+	for (i = 0; i < (index - 1); i++)
+	{
+		if (tmp->next == NULL)
+			return (-1);
+		tmp = tmp->next;
+	}
+	new = tmp->next;
+	tmp->next = new->next;
+	free(new);
+	return (-1);
+}
