ddcb33de700a9f325a1a62421a890f07fc1a3528
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-05-02 19:22:38 -0500

    Update

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,6 +10,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *tmp = *head;
 	dlistint_t *new;
+	dlistint_t *ant;
 	unsigned int i;
 
 	if (tmp == NULL)
@@ -28,6 +29,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	new = tmp->next;
 	tmp->next = new->next;
+	ant = tmp->prev;
+	ant->next = tmp;
 	free(new);
 	return (1);
 }
