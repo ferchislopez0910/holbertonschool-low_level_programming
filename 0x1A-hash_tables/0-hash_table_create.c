c69aeaa2203443e4fc2436200ee3abf6bc56f1fe
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-05-28 05:06:45 -0500

    Put the if statment, when the function fail!

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,11 +10,13 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = NULL;
-	unsigned int iterador = 0;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
+
+	if (!new_table)
+		return NULL;
 
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
