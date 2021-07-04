f7351b6444042f9921514440e6644319ba0943ec
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-07-04 16:49:41 -0500

    Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Create a function def island_perimeter
+(grid): that returns the 
+perimeter of the island described in grid"""from _typeshed import HasFileno
+from typing import Counter
+
+
+}
+
+def island_perimeter(grid):
+	high= 0
+	weigh= 0
+
+	for high in range(len(grid)):
+		for weigh in range(len(grid[high])):
+			if grid[high][weigh] == 1:
+				counter += 1
+				if high != 0 and grid[high -1][weigh] == 1:
+					cont2 += 1
+				if weigh != 0 and grid[high][weigh - 1] == 1:
+					cont2 += 1
+	return (4 * counter) - (2 * cont2)
